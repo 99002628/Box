698607e7271293c78dea418de84603dfc9851bb5
99002628 <71540868+99002628@users.noreply.github.com>
2020-10-05 14:20:30 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,52 @@
+#include "pch.cpp"
+
+#include<iostream>
+#include<gtest/gtest.h>
+using namespace std;
+
+
+TEST(Box, DefaultConstructor)
+{
+	Box b;
+	EXPECT_EQ(0, b.length());
+	EXPECT_EQ(0, b.height());
+	EXPECT_EQ(0, b.breadth());
+
+}
+TEST(Box, ParameterizedConstructor_1)
+{
+	Box b(30);
+	EXPECT_EQ(30, b.length());
+	EXPECT_EQ(30, b.height());
+	EXPECT_EQ(30, b.breadth());
+
+}
+TEST(Box, ParameterizedConstructor_2)
+{
+	Box b(10, 20, 30);
+	EXPECT_EQ(10, b.length());
+	EXPECT_EQ(20, b.height());
+	EXPECT_EQ(30, b.breadth());
+
+}
+TEST(Box, CopyConstructor)
+{
+	Box b1(10, 20, 30);
+	Box b(b1);
+	EXPECT_EQ(10, b.length());
+	EXPECT_EQ(20, b.height());
+	EXPECT_EQ(30, b.breadth());
+
+}
+TEST(Box, Volume)
+{
+	Box b(10, 20, 30);
+	EXPECT_EQ(6000, b.volume());
+
+}
+int main(int argc, char **argv) 
+    {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    }
+
